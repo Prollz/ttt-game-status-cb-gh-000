e9086b4340806246e3ca00164eafc00197f851d0
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-26 10:57:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,6 +20,7 @@ def won?(board)
     position_2 = board[win_combination[1]]
     position_3 = board[win_combination[2]]
 
-    (position_1 == "X" && position_2 == "X" && position_3 == "X") ||   (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    (position_1 == "X" && position_2 == "X" && position_3 == "X")
+    || (position_1 == "O" && position_2 == "O" && position_3 == "O")
   end
 end
