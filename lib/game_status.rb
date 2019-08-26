a7b94b9df7e674098776ea8c6bbe6c1854554ca5
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-26 10:55:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won(board)
     position_3 = board[win_combination[2]]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combination
+      true
     else
       false
     end
